60b06c992f53cacf22d755a06f776ce477cb2066
Baizid Bostami <mdbaizidbostami196@gmail.com>
2026-06-24 21:31:18 +0600

    replace the localhost http

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "https://cattle-x-back-end.vercel.app";
 
 class ApiService {
   private baseUrl: string;
